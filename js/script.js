742b3b44fb71679baf54ca9fae868b9195842fc0
Pedro Henrique Dias de Souza <rodrigojponte@hotmail.com>
2025-07-03 17:26:46 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
 $(function() {
 
+    marcarHomeComoAtiva();
+    carregarHome();
     verificarCliqueMenu();
 
-    function verificarCliqueMenu() {
-         $('a').click(function() {
-              
-            var href = $(this).attr('href');
 
-            $.ajax({
+      function marcarHomeComoAtiva() {
+              $('nav ul li a').removeClass('active');
+              $('#home-link').addClass('active');
+      }
+
+      function carregarHome() {
+          $.ajax({
+             'url': 'home.html',
+             'timeout':10000,
+             'erro': function(jqXHR, textStatus, errorThrown) {
+                if(jqXHR.statusText == 'Not Found') {
+                    console.log("Página Home não encontrada.");
+                    $('#container').html('<p>Página HOME não encontrada.</p>');
+                }
+             }
+
+          }).done(function(data) {
+             $('#container').html('');
+             $(data).appendTo('#container').fadeIn();
+          })
+      }
+
+      function verificarCliqueMenu() {
+          $('nav ul li a').click(function() {
+             $('nav ul li a').removeClass('active');
+
+             $(this).addClass('active');
+
+             var href = $(this).attr('href');
+
+             $.ajax({
                 'url':href,
                 'timeout':10000,
-                'error':function(jqXHR, textStatus,errorThrown) {
-                   if(jqXHR.statusText == 'Not Found') {
-                     console.log("Página não encontrada.");
-                   }
+                'error': function(jqXHR, textStatus, errorThrown) {
+                     if(jqXHR.statusText == 'Not Found') {
+                        console.log("Página não encontrada");
+                     }
                 }
-                
-            }).done(function(data){
-                //$('#container').html(data);
-                $(data).appendTo('#container').fadeIn();
-            });
+             }).done(function(data) {
+                 $('#container').html('');
+                 $(data).appendTo('#container').fadeIn();
+             });
 
-            return false;
-         });
-    }
+             return false;
+          });
+      }
 });
